9c32e6b6d380a60d675897287a4e475f2d0e5238
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-03-25 23:26:00 +0530

    navbar component

---
+++ app/components/Button.jsx
@@ -0,0 +1,6 @@
+const Button = (props) => {
+  return (
+    <div className="bg-[#7E6363] hover:bg-[#A87C7C] px-3 py-1 rounded-full shadow-2xl hover:-translate-y-0.5 tracking-wide font-medium">{props.value}</div>
+  )
+}
+export default Button
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { FaBrain } from "react-icons/fa";
+import Button from "./Button";
+
+const checkAuthentication = () => {
+  return false;
+};
+
+const Navbar = () => {
+  const authenticated = checkAuthentication();
+
+  return (
+    <nav className="w-full px-10 pt-4 sm:textsm">
+      <div className="bg-[#3E3232] flex justify-between items-center mx-auto px-10 py-3 max-sm:px-8 rounded-full shadow-xl">
+        <div className="flex gap-x-3">
+          <Link href="/">
+            <FaBrain className="w-6 h-6" />
+          </Link>
+          <h1 className="text-xl font-bold tracking-wide">BlogPress</h1>
+        </div>
+        <div>
+          {authenticated ? (
+            <Link href="/api/logout">
+              <Button value="Logout" />
+            </Link>
+          ) : (
+            <div className="flex gap-3">
+              <Link href="/api/login">
+                <Button value="Login" />
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
